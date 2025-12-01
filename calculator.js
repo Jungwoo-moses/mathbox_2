cd662a3534c2ea6fe6b128c1315d874fa5c2da86
jungwoo <jungwoo7601@gmail.com>
2025-12-01 13:10:40 +0900

    Change divide for premium

--- calculator.js
+++ calculator.js
@@ -3,10 +3,17 @@
 const add=( a, b )=> a+b;
 //type (x: number, y:number)->number;
 const substract = (a, b)=> a -b;
+//
 // type (x:number, y: number)->number;
 const multiply = (a ,b) =>{ return a *b
 }; 
+// type (x: number, y:number)->number;
+const divide =(a, b)=> a / b;{
+  if(b===0) return;
+  return a/b;}
 
+  const divide_for_premium=(a, b)=> a/b;
+  
 //git init 로컬 레포지토리 만들기
 //git add 스테이징으로 옮기기
 //git comit 로컬레포지토리 저장하기
