f38735f83403c3e664fca89718993fd428e2987e
jungwoo <jungwoo7601@gmail.com>
2025-12-01 09:26:45 +0900

    Update multiply func

---
+++ calculator.js
@@ -0,0 +1,13 @@
+
+//type (x: number, y:number)-> number;
+const add=( a, b )=> a+b;
+//type (x: number, y:number)->number;
+const substract = (a, b)=> a -b;
+// type (x:number, y: number)->number;
+const multiply = (a ,b) =>{ return a *b
+}; 
+
+//git init 로컬 레포지토리 만들기
+//git add 스테이징으로 옮기기
+//git comit 로컬레포지토리 저장하기
+//git push  로컬레포지토리와 리모트 레포지토리 동기화 하기
